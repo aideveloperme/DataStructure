8f136080363b87ad70e6bc53f977a6cb35aa495d
MeghaK <163288008+aideveloperme@users.noreply.github.com>
2025-07-27 14:38:19 +0530

    Update LinkedList.cpp

--- linkedlist/insertatbegin.cpp
+++ linkedlist/insertatbegin.cpp
@@ -1,199 +1,245 @@
 #include<iostream>
+#include<vector>
 using namespace std;
- struct Node{
-	 int data;
-	 Node* next;
-}
-void insteratbegin(int data){       //passing int data as argument
-	Node* newNode= new Node();  //create space in memeory
-	newNode->data=data;         //insert data into newnode data field
-	newNode->next=head;         //inorder to insert first node , newnode will strore the address of the next node , so which is in the head node ,
-	head=newNode;		    //so put head value inside the next part of newnode & head node set as newnode :head it will store the addr of newnode
+
+// Define a Node of linked list
+struct Node{
+    int data;       // data part
+    Node* next;     // pointer to next node
+    Node(int val) : data(val), next(NULL) {} // constructor
+};
+
+// Global head pointer, initially NULL
+Node* head = NULL;
+
+// Insert node at beginning
+void insertAtBegin(int data){       // pass data to insert
+    Node* newNode = new Node(data); // create new node with given data
+    newNode->next = head;           // new node points to old head
+    head = newNode;                 // move head to new node
 }
 
-void printlist(){             //print function -no argument
-	Node* temp =head;           //first we store the head value into temp pointer variable ,so temp will point to head 
-	while(temp!=NULL){          // check temp  null or not if null emptylist return 0 else it has element to traverse
-		cout<<temp->data<<"";  // so it will print the temp->data temp data that is first data
-		temp=temp->next;       // and temp will increment by one.. temp->next is store into temp .
-	}
-
-}
-Node *insertend(Node *head,int X){  //passing head and data item to be insert why bcz,through head only we can acces ll
-	Node *temp=new Node(X);     //create a memory contains data X
-	if(head==NULL)return temp;  // if head part is null ,, no element , ll empty ,so return newnode
-	Node *curr=head;            //store head value into curr pointer variable
-	while(curr->next!=NULL){    // first elem is  not null , increament curr and if it is reaches null
-		curr=curr->next;
-	}
-	curr->next=temp;           // store newnode address that is temp holding ls into curr next part
-	return head;               //return the head , points to head 
-}
-Node *deleteHead(Node *head){
-	if(head==NULL)return NULL;
-	else
-		Node *temp=head->next;
-		delete(head);
-		return temp;
-	}
+// Print linked list
+void printList(){
+    Node* temp = head;             // temp points to head
+    while(temp != NULL){           // traverse till end
+        cout << temp->data << " "; // print data
+        temp = temp->next;         // move to next node
+    }
+    cout << endl;
 }
 
-Node *deleteLast(Node *head){       //passing head  pointer as argument 
-	if(head==NULL)return NULL;  // if head is null ,list is empty so return null
-	if(head->next=NULL){       //if list having only one element then delete head which is storing the addr of the first element 
-		delete head;       
-		return NULL;
-	}
-	Node *temp=head;                //store head value into temp pointer variable of type node 
-	while(temp->next->next!==NULL){ // until the current next next value is NULL loop will go and increment the temp pointer varible 
-		temp=temp->next;
-	}
-	delete(temp->next);          // delete temp->next means addres part of the first last node and set temp->next as null
-	temp->next=NULL;
-	return head;
+// Insert node at end
+Node* insertEnd(Node* head, int X){
+    Node* newNode = new Node(X);       // create new node
+    if(head == NULL) return newNode;   // if list empty, new node is the list
+    Node* curr = head;                 // start from head
+    while(curr->next != NULL){         // move to last node
+        curr = curr->next;
+    }
+    curr->next = newNode;              // link new node at end
+    return head;                       // return unchanged head
 }
 
-void insertATpoistion(Node *head,int X,int pos){
-	Node *newNode= new Node(4);
-	if(head==NULL)
-		if(pos==1)return temp;
-		else return head;
-	}
-	Node *temp=head;
-	if(pos==1)
-		temp->next=head;
-
-
-	for (int i=1;i<pos-1;i++){
-		temp=temp->next;
-		if(temp==NULL){ //NO POSITION
-			cout<<"position out of range"<< endl;
-			return head;
-
-	}
-	}
-	temp->next=temp;
-	newNode->next=temp->next->next;
-
-
+// Delete head node
+Node* deleteHead(Node* head){
+    if(head == NULL) return NULL;   // empty list
+    Node* temp = head->next;        // save next node
+    delete head;                    // free memory of old head
+    return temp;                    // return new head
 }
 
-int search(Node * head, int X){
-	int pos=1;
-	Node *curr=head;
-	while(curr!=NULL)
-		if(curr->data==searchdata)
-			return pos;
-		else 
-			pos++;
-			curr=curr->next;
-	return -1;
+// Delete last node
+Node* deleteLast(Node* head){
+    if(head == NULL) return NULL;          // empty list
+    if(head->next == NULL){                // only one node
+        delete head;
+        return NULL;
+    }
+    Node* curr = head;                     // start from head
+    while(curr->next->next != NULL){       // move to second last node
+        curr = curr->next;
+    }
+    delete curr->next;                     // delete last node
+    curr->next = NULL;                     // second last becomes last
+    return head;
 }
-int search(Node *head,int X){
-	int pos=1;
-	Node *curr=head;
-	while(curr!=NULL)
-		if(curr->data==X)
-			return pos;
-		else 
-			pos++;
-			curr=curr->next;
-	return -1;
-}
-int recursivesreach(Node * head, int X){
-	if(head==NULL)return -1;
-	if(head->data==X)return 1;
-	else
-		int res=recursivsearch(head->next,X);
-		if(res==-1)return -1;// if data is not there and head-/.next is null return -1 so if res is -1 return -1 to all recursive call 
-		else return res+1;
-}
-}
-//search fun time is O(n) whether is recursive or iterative 
-//
-void printMiddle(Node *head){
-	if(head==NULL)return;
-	int count=0;
-	Node *curr;
-	for (curr=head;curr!=NULL;curr=curr->next)
-		count++;
-	curr=head;
-	for(int i=0;i<count/2;i++)
-		curr=curr->next;
-	cout<<curr->data;
-}
-//this require 2 traversal of linked list
-//
-//now we have 1 traversal of ll solution using 2 pointers based on the idea od slow and fast pointer 
-int PrintMiddle(Node *head){
-	if(head==NULL) return -1;
-	Node *slow=head;
-	Node *fast=head;
 
-	while(fast!=NULL && fast->next!=NULL){//when fast become Null or fast next becomes null at that time slow will refering to the middle node 
-		slow=slow->next;
-		fast=fast->next->next;
-	}
-	cout<<slow->data;
+// Insert at position (1-based)
+Node* insertAtPosition(Node* head, int X, int pos){
+    Node* newNode = new Node(X);           // create new node
+    if(pos == 1){                          // insert at head
+        newNode->next = head;
+        return newNode;
+    }
+    Node* temp = head;
+    for(int i=1; i<pos-1; i++){            // move to (pos-1)-th node
+        if(temp == NULL){
+            cout << "Position out of range" << endl;
+            return head;
+        }
+        temp = temp->next;
+    }
+    if(temp == NULL){
+        cout << "Position out of range" << endl;
+        return head;
+    }
+    newNode->next = temp->next;            // link new node
+    temp->next = newNode;
+    return head;
+}
 
+// Iterative search: return position or -1
+int search(Node* head, int X){
+    int pos = 1;
+    Node* curr = head;
+    while(curr != NULL){
+        if(curr->data == X)
+            return pos;
+        curr = curr->next;
+        pos++;
+    }
+    return -1;
 }
-//Nth Node from end of linked list 
-int printNthFromend(Node * head,int n){
-	int len=0;
-	for(Node *curr=head;curr!=NULL;curr=curr->next)
-		len++;
-	if(len<n);
-	return;
-	Node *curr=head;
-	for(int i=1;i<len-n+1;i++)
-		curr=curr->next;
-	cout<<(curr->data)<<"";
-}
-//using 2 pointer 
-int printNth(Node *head, int n)
-{	Node *first=head;
-	Node *seond=head;
-	if(head==NULL)return;
-	for(int i=0;i<n;i++){
-		if(first==NULL)return;
-		first=first->next;
-	}
-	while(first!=NULL){
-	second=second->next;
-	first=first->next;
-	}
-cout<<(second->data);
-}
-//reverse ll-naive method
-Node *revList(Node *head)
-	vector<int> arr;
-	for(Node *curr=head;curr!=NULL;curr=curr->next){
-		arr.push_back(curr->data);
-	}
-	for(Node *curr=head;curr!=NULL;curr=curr->next){
-		curr->data=arr.back();
-		arr.pop_back();
-	}
-return head;
-}
-//efficeint code
-Node *reverse(Node *head){
-	Node *curr=head;
-	Node *prev=NULL:
-	while(curr!=NULL){
-		Node *node
 
+// Recursive search: return position or -1
+int recursiveSearch(Node* head, int X){
+    if(head == NULL) return -1;                // not found
+    if(head->data == X) return 1;              // found at first node
+    int res = recursiveSearch(head->next, X);  // search in rest
+    if(res == -1) return -1;                    // not found in rest
+    else return res + 1;                        // add 1 to position
 }
-//recursive solution
 
+// Print middle using count (two traversals)
+void printMiddle(Node* head){
+    if(head == NULL) return;
+    int count = 0;
+    Node* curr = head;
+    while(curr != NULL){               // count nodes
+        count++;
+        curr = curr->next;
+    }
+    curr = head;
+    for(int i=0; i<count/2; i++)       // go to middle
+        curr = curr->next;
+    cout << "Middle element: " << curr->data << endl;
+}
+
+// Print middle using slow & fast pointers (one traversal)
+void printMiddleEfficient(Node* head){
+    if(head == NULL) return;
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    cout << "Middle element: " << slow->data << endl;
+}
+
+// Print nth node from end (two traversals)
+void printNthFromEnd(Node* head, int n){
+    int len=0;
+    Node* curr=head;
+    while(curr!=NULL){         // count length
+        len++;
+        curr=curr->next;
+    }
+    if(len<n){
+        cout << "Position out of range" << endl;
+        return;
+    }
+    curr = head;
+    for(int i=1;i<len-n+1;i++) // move to (len-n+1)-th node
+        curr=curr->next;
+    cout << n << "th node from end: " << curr->data << endl;
+}
+
+// Print nth node from end (one traversal)
+void printNth(Node* head, int n){
+    Node* first=head;
+    Node* second=head;
+    for(int i=0;i<n;i++){
+        if(first==NULL){
+            cout << "Position out of range" << endl;
+            return;
+        }
+        first=first->next;
+    }
+    while(first!=NULL){
+        first=first->next;
+        second=second->next;
+    }
+    cout << n << "th node from end: " << second->data << endl;
+}
+
+// Reverse list (naive: data changes)
+Node* revList(Node* head){
+    vector<int> arr;
+    for(Node* curr=head; curr!=NULL; curr=curr->next)
+        arr.push_back(curr->data);
+    for(Node* curr=head; curr!=NULL; curr=curr->next){
+        curr->data=arr.back();
+        arr.pop_back();
+    }
+    return head;
+}
+
+// Reverse list (efficient: pointer changes)
+Node* reverse(Node* head){
+    Node* prev=NULL;
+    Node* curr=head;
+    while(curr!=NULL){
+        Node* next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
+}
+
+// Recursive reverse
+Node* recursiveReverse(Node* head){
+    if(head==NULL || head->next==NULL)
+        return head;
+    Node* rest=recursiveReverse(head->next);
+    head->next->next=head;
+    head->next=NULL;
+    return rest;
+}
 
 int main(){
-	insertatbegin(1);
-	insertatbegin(2);
-	printlist();
-	deleteHead(head);
-	return 0;
+    insertAtBegin(1);           // insert 1 at beginning
+    insertAtBegin(2);           // insert 2 at beginning
+    insertAtBegin(3);           // insert 3 at beginning
+    printList();                // list: 3 2 1
+
+    head = insertEnd(head, 10); // insert 10 at end
+    printList();                // list: 3 2 1 10
+
+    head = deleteHead(head);    // delete first node
+    printList();                // list: 2 1 10
+
+    head = deleteLast(head);    // delete last node
+    printList();                // list: 2 1
+
+    head = insertAtPosition(head, 5, 2); // insert 5 at position 2
+    printList();                        // list: 2 5 1
+
+    cout << "Search 5: " << search(head, 5) << endl; // find position
+
+    printMiddle(head);                  // print middle
+    printMiddleEfficient(head);         // print middle (efficient)
+
+    printNthFromEnd(head, 2);           // nth from end
+    printNth(head, 2);                  // nth from end (one pass)
+
+    head = reverse(head);               // reverse list
+    printList();
+
+    head = recursiveReverse(head);      // reverse again (recursive)
+    printList();
+
+    return 0;
 }
-
-
-
